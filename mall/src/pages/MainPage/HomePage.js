177dcc8945b33c92bd79083d3339dd60aca04229
Tuziking <108811704+Tuziking@users.noreply.github.com>
2024-05-29 23:28:34 +0800

    添加了部分的图片

--- mall/src/pages/MainPage/HomePage.js
+++ mall/src/pages/MainPage/HomePage.js
@@ -1,58 +1,76 @@
 import React from 'react';
 import {
-  Carousel, Input, Card, List, Button,
+    Carousel, Input, Card, List, Button,
 } from 'antd';
-import { withRouter } from 'react-router-dom';
+import {withRouter} from 'react-router-dom';
 import specialIcon from './icons/special.png';
 import eventIcon from './icons/special.png';
 import discountIcon from './icons/special.png';
 import promotionIcon from './icons/special.png';
+import {Tabs} from "antd";
+import {Space} from "antd-mobile";
 
-const { Search } = Input;
+const {Search} = Input;
+const { TabPane } = Tabs;
 
 const hotItems = [
-  { title: '小米12 Pro', price: '¥2999' },
-  { title: 'Redmi K50', price: '¥2099' },
+    {title: '小米12 Pro', price: '¥2999'},
+    {title: 'Redmi K50', price: '¥2099'},
 ];
 
-const HomePage = ({ history }) => {
-  const handleSearch = (value) => {
-    history.push({
-      pathname: '/mainpage/products',
-      state: { searchText: value },
-    });
-  };
+const HomePage = ({history}) => {
+    const handleSearch = (value) => {
+        history.push({
+            pathname: '/mainpage/products',
+            state: {searchText: value},
+        });
+    };
 
-  return (
-    <div>
-      <Search placeholder="请输入商品名称 如: 手机" onSearch={handleSearch} enterButton />
+    return (
+        <div>
+            <Space direction='vertical'  style={{ width: '100%' }}>
+                <Search placeholder="请输入商品名称 如: 手机" onSearch={handleSearch} enterButton/>
+                <Carousel autoplay>
+                    <div><img src="https://res.vmallres.com/uomcdn/CN/cms/202405/ee061dafcb20437aa6e58e174e235c2c.jpg"
+                              alt="HUAWEI Pura70系列"/></div>
+                    <div><img src="https://res.vmallres.com/uomcdn/CN/cms/2024-05/5495c7a8405d47998f9804521dd500f8.jpg"
+                              alt="华为Vision智慧屏4"/></div>
+                    <div><img src="https://res.vmallres.com/uomcdn/CN/cms/2024-05/f1a956d19f5c44a7ab1f2a23db1eeae0.jpg"
+                              alt="HUAWEI MateBook X Pro"/></div>
+                </Carousel>
+                <Tabs defaultActiveKey="1" centered>
+                    <TabPane tab="专题" key="1">
+                        {/*<div>Content of 专题</div>*/}
+                    </TabPane>
+                    <TabPane tab="活动" key="2">
+                        {/*<div>Content of 活动</div>*/}
+                    </TabPane>
+                    <TabPane tab="优惠" key="3">
+                        {/*<div>Content of 优惠</div>*/}
+                    </TabPane>
+                    <TabPane tab="特惠" key="4">
+                        {/*<div>Content of 特惠</div>*/}
+                    </TabPane>
+                </Tabs>
+                <Card title="品牌制造商直供">
+                    <List
+                        grid={{gutter: 16, column: 2}}
+                        dataSource={hotItems}
+                        renderItem={(item) => (
+                            <List.Item>
+                                <Card title={item.title}>{item.price}</Card>
+                            </List.Item>
+                        )}
+                    />
+                </Card>
+            </Space>
 
-      <Carousel autoplay>
-        <div><img src="轮播图1.png" alt="轮播图1" /></div>
-        <div><img src="轮播图2.png" alt="轮播图2" /></div>
-        <div><img src="轮播图3.png" alt="轮播图3" /></div>
-      </Carousel>
 
-      <div className="categories">
-        <Button icon={<img src={specialIcon} alt="专题" />}>专题</Button>
-        <Button icon={<img src={eventIcon} alt="活动" />}>活动</Button>
-        <Button icon={<img src={discountIcon} alt="优惠" />}>优惠</Button>
-        <Button icon={<img src={promotionIcon} alt="特惠" />}>特惠</Button>
-      </div>
 
-      <Card title="品牌制造商直供">
-        <List
-          grid={{ gutter: 16, column: 2 }}
-          dataSource={hotItems}
-          renderItem={(item) => (
-            <List.Item>
-              <Card title={item.title}>{item.price}</Card>
-            </List.Item>
-          )}
-        />
-      </Card>
-    </div>
-  );
+
+
+        </div>
+    );
 };
 
 export default withRouter(HomePage);
--- mall/src/pages/MainPage/MyPage.js
+++ mall/src/pages/MainPage/MyPage.js
@@ -1,212 +1,206 @@
 import React, { useState, useEffect } from 'react';
 import {
-  List, Card, Button, message,
+    List, Card, Button, message,
 } from 'antd';
 import { withRouter } from 'react-router-dom';
-import {
-  reqGetAvailableCoupons, reqClaimCoupon, reqGetUserCoupons, reqGetOrders,
-} from 'api';
 import { connect } from 'react-redux';
-// 1
+import {
+    reqGetAvailableCoupons, reqClaimCoupon, reqGetUserCoupons, reqGetOrders,
+} from 'api';
+import {
+    ShoppingCartOutlined,
+    DollarOutlined,
+    TagsOutlined,
+    SyncOutlined,
+    CloseCircleOutlined,
+    EnvironmentOutlined,
+    UserSwitchOutlined,
+    HeartOutlined,
+    StarOutlined,
+    MessageOutlined
+} from '@ant-design/icons';
+import {Space} from "antd-mobile";
+
 const MyPage = ({ history, userInfo }) => {
-  const [availableCoupons, setAvailableCoupons] = useState([]);
-  const [userCoupons, setUserCoupons] = useState([]);
-  const [orders, setOrders] = useState([]);
+    const [availableCoupons, setAvailableCoupons] = useState([]);
+    const [userCoupons, setUserCoupons] = useState([]);
+    const [orders, setOrders] = useState([]);
 
-  useEffect(() => {
-    fetchAvailableCoupons();
-    fetchUserCoupons();
-    fetchOrders();
-  }, []);
-
-  const fetchAvailableCoupons = async () => {
-    try {
-      const res = await reqGetAvailableCoupons();
-      if (res.status === 0) {
-        setAvailableCoupons(res.data);
-      } else {
-        message.error('获取可领取优惠券失败');
-      }
-    } catch (error) {
-      message.error('获取可领取优惠券失败');
-    }
-  };
-
-  const fetchUserCoupons = async () => {
-    try {
-      const res = await reqGetUserCoupons({ userId: userInfo._id });
-      if (res.status === 0) {
-        setUserCoupons(res.data);
-      } else {
-        message.error('获取用户优惠券失败');
-      }
-    } catch (error) {
-      message.error('获取用户优惠券失败');
-    }
-  };
-
-  const fetchOrders = async () => {
-    const userId = userInfo._id;
-    try {
-      const res = await reqGetOrders({ userId });
-      setOrders(res.data);
-    } catch (error) {
-      message.error('获取订单信息失败');
-    }
-  };
-
-  const handleClaimCoupon = async (couponId) => {
-    try {
-      const res = await reqClaimCoupon({ userId: userInfo._id, couponId });
-      if (res.status === 0) {
-        message.success('优惠券领取成功');
+    useEffect(() => {
         fetchAvailableCoupons();
         fetchUserCoupons();
-      } else {
-        message.error('领取优惠券失败');
-      }
-    } catch (error) {
-      message.error('领取优惠券失败');
-    }
-  };
+        fetchOrders();
+    }, []);
 
-  const handleOrderClick = (status) => {
-    const filteredOrders = status === 'all' ? orders : orders.filter((order) => {
-      if (status === '退款/售后') {
-        return order.status.includes('退款') || order.status.startsWith('售后处理中');
-      }
-      return order.status === status;
-    });
-    history.push({
-      pathname: '/order',
-      state: { orders: filteredOrders },
-    });
-  };
+    const fetchAvailableCoupons = async () => {
+        try {
+            const res = await reqGetAvailableCoupons();
+            if (res.status === 0) {
+                setAvailableCoupons(res.data);
+            } else {
+                message.error('获取可领取优惠券失败');
+            }
+        } catch (error) {
+            message.error('获取可领取优惠券失败');
+        }
+    };
 
-  const handlePageNavigation = (page) => {
-    history.push(page);
-  };
+    const fetchUserCoupons = async () => {
+        try {
+            const res = await reqGetUserCoupons({ userId: userInfo._id });
+            if (res.status === 0) {
+                setUserCoupons(res.data);
+            } else {
+                message.error('获取用户优惠券失败');
+            }
+        } catch (error) {
+            message.error('获取用户优惠券失败');
+        }
+    };
 
-  const menuItems = [
-    { title: '全部订单', icon: 'order-icon-url', status: 'all' },
-    { title: '待付款', icon: 'payment-icon-url', status: '待付款' },
-    { title: '待发货', icon: 'delivery-icon-url', status: '待发货' },
-    { title: '待收货', icon: 'shipping-icon-url', status: '待收货' },
-    { title: '退换售后', icon: 'aftersale-icon-url', status: '退款/售后' },
-    { title: '已取消', icon: 'cancel-icon-url', status: '已取消' },
-  ];
+    const fetchOrders = async () => {
+        const userId = userInfo._id;
+        try {
+            const res = await reqGetOrders({ userId });
+            setOrders(res.data);
+        } catch (error) {
+            message.error('获取订单信息失败');
+        }
+    };
 
-  const otherItems = [
-    { title: '地址管理', icon: 'address-icon-url', page: '/address' },
-    { title: '我的足迹', icon: 'footprint-icon-url', page: '/footprint' },
-    { title: '我的关注', icon: 'favorites-icon-url', page: '/favorites' },
-    { title: '我的收藏', icon: 'collection-icon-url', page: '/favorite' },
-    { title: '我的评价', icon: 'reviews-icon-url', page: '/reviews' },
-  ];
+    const handleClaimCoupon = async (couponId) => {
+        try {
+            const res = await reqClaimCoupon({ userId: userInfo._id, couponId });
+            if (res.status === 0) {
+                message.success('优惠券领取成功');
+                fetchAvailableCoupons();
+                fetchUserCoupons();
+            } else {
+                message.error('领取优惠券失败');
+            }
+        } catch (error) {
+            message.error('领取优惠券失败');
+        }
+    };
 
-  return (
-    <Card title="我的页面">
-      <div className="user-info">
-        <img src="user-avatar-url" alt="用户头像" />
-        <div>
-          <div>会员</div>
-          <div>
-            积分:
-            {userInfo.points}
-            {' '}
-            成长值:
-            {userInfo.growth}
-          </div>
-        </div>
-      </div>
-      <Card title="可领取优惠券">
-        <List
-          itemLayout="vertical"
-          dataSource={availableCoupons}
-          renderItem={(coupon) => (
-            <List.Item key={coupon._id}>
-              <Card>
-                <p>
-                  优惠券代码:
-                  {coupon.code}
-                </p>
-                <p>
-                  优惠金额: ¥
-                  {coupon.discount}
-                </p>
-                <p>
-                  最低消费: ¥
-                  {coupon.minSpend}
-                </p>
-                <p>
-                  有效期:
-                  {new Date(coupon.expiryDate).toLocaleDateString()}
-                </p>
-                <Button onClick={() => handleClaimCoupon(coupon._id)}>领取</Button>
-              </Card>
-            </List.Item>
-          )}
-        />
-      </Card>
-      <Card title="我的优惠券">
-        <List
-          itemLayout="vertical"
-          dataSource={userCoupons}
-          renderItem={(coupon) => (
-            <List.Item key={coupon._id}>
-              <Card>
-                <p>
-                  优惠券代码:
-                  {coupon.code}
-                </p>
-                <p>
-                  优惠金额: ¥
-                  {coupon.discount}
-                </p>
-                <p>
-                  最低消费: ¥
-                  {coupon.minSpend}
-                </p>
-                <p>
-                  有效期:
-                  {new Date(coupon.expiryDate).toLocaleDateString()}
-                </p>
-                <p>{coupon.isClaimed ? '已领取' : '未领取'}</p>
-              </Card>
-            </List.Item>
-          )}
-        />
-      </Card>
-      <List
-        grid={{ gutter: 16, column: 5 }}
-        dataSource={menuItems}
-        renderItem={(item) => (
-          <List.Item>
-            <Card
-              title={item.title}
-              onClick={() => handleOrderClick(item.status)}
-            >
-              <img src={item.icon} alt={item.title} />
-            </Card>
-          </List.Item>
-        )}
-      />
-      <List
-        dataSource={otherItems}
-        renderItem={(item) => (
-          <List.Item>
-            <Card title={item.title} onClick={() => handlePageNavigation(item.page)}>
-              <img src={item.icon} alt={item.title} />
-            </Card>
-          </List.Item>
-        )}
-      />
-    </Card>
-  );
+    const handleOrderClick = (status) => {
+        const filteredOrders = status === 'all' ? orders : orders.filter((order) => {
+            if (status === '退款/售后') {
+                return order.status.includes('退款') || order.status.startsWith('售后处理中');
+            }
+            return order.status === status;
+        });
+        history.push({
+            pathname: '/order',
+            state: { orders: filteredOrders },
+        });
+    };
+
+    const handlePageNavigation = (page) => {
+        history.push(page);
+    };
+
+    const menuItems = [
+        { title: '全部订单', icon: <ShoppingCartOutlined />, status: 'all' },
+        { title: '待付款', icon: <DollarOutlined />, status: '待付款' },
+        { title: '待发货', icon: <TagsOutlined />, status: '待发货' },
+        { title: '待收货', icon: <SyncOutlined />, status: '待收货' },
+        { title: '退换', icon: <CloseCircleOutlined />, status: '退款/售后' },
+        { title: '已取消', icon: <CloseCircleOutlined />, status: '已取消' },
+    ];
+
+    const otherItems = [
+        { title: '地址管理', icon: <EnvironmentOutlined />, page: '/address' },
+        { title: '我的足迹', icon: <UserSwitchOutlined />, page: '/footprint' },
+        { title: '我的关注', icon: <HeartOutlined />, page: '/favorites' },
+        { title: '我的收藏', icon: <StarOutlined />, page: '/favorite' },
+        { title: '我的评价', icon: <MessageOutlined />, page: '/reviews' },
+    ];
+
+    return (
+        <Card title="我的页面">
+            <Space direction='vertical' >
+                <div className="user-info">
+                    <img src="user-avatar-url" alt="用户头像"/>
+                    <div>
+                        <div>会员</div>
+                        <div>
+                            积分: {userInfo.points} 成长值: {userInfo.growth}
+                        </div>
+                    </div>
+                </div>
+                <Card title="可领取优惠券" style={{width:'100%'}}>
+                    <List
+                        itemLayout="vertical"
+                        dataSource={availableCoupons}
+                        renderItem={(coupon) => (
+                            <List.Item key={coupon._id}>
+                                <Card>
+                                    <p>优惠券代码: {coupon.code}</p>
+                                    <p>优惠金额: ¥{coupon.discount}</p>
+                                    <p>最低消费: ¥{coupon.minSpend}</p>
+                                    <p>有效期: {new Date(coupon.expiryDate).toLocaleDateString()}</p>
+                                    <Button onClick={() => handleClaimCoupon(coupon._id)}>领取</Button>
+                                </Card>
+                            </List.Item>
+                        )}
+                    />
+                </Card>
+                <Card title="我的优惠券" style={{width:'100%'}}>
+                    <List
+                        itemLayout="vertical"
+                        dataSource={userCoupons}
+                        renderItem={(coupon) => (
+                            <List.Item key={coupon._id}>
+                                <Card>
+                                    <p>优惠券代码: {coupon.code}</p>
+                                    <p>优惠金额: ¥{coupon.discount}</p>
+                                    <p>最低消费: ¥{coupon.minSpend}</p>
+                                    <p>有效期: {new Date(coupon.expiryDate).toLocaleDateString()}</p>
+                                    <p>{coupon.isClaimed ? '已领取' : '未领取'}</p>
+                                </Card>
+                            </List.Item>
+                        )}
+                    />
+                </Card>
+                <Card style={{width:'100%'}}>
+                    <List
+                        grid={{gutter: 16, column: 5}}
+                        dataSource={menuItems}
+                        renderItem={(item) => (
+                            <List.Item>
+                                <Card title={item.title} onClick={() => handleOrderClick(item.status)}
+                                      style={{textAlign: 'center'}}>
+                                    {item.icon}
+                                </Card>
+                            </List.Item>
+                        )}
+                    />
+                </Card>
+
+                <Card>
+                    <List
+                        grid={{gutter: 16, column: 5}}
+                        dataSource={otherItems}
+                        renderItem={(item) => (
+                            <List.Item>
+                                <Card title={item.title} onClick={() => handlePageNavigation(item.page)}
+                                      style={{textAlign: 'center'}}>
+                                    {item.icon}
+                                </Card>
+                            </List.Item>
+                        )}
+                    />
+                </Card>
+
+            </Space>
+
+        </Card>
+    );
 };
 
 const mapStateToProps = (state) => ({
-  userInfo: state.loginUserInfo,
+    userInfo: state.loginUserInfo,
 });
 
 export default connect(mapStateToProps)(withRouter(MyPage));
