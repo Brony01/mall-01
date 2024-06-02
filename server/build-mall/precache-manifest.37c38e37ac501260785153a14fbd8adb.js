fe2ac136db7a837edf24d45749877a9b7340432f
hys <21301039@bjtu.edu.cn>
2024-06-02 12:17:53 +0800

    重新修复了启动配置

---
+++ server/build-mall/precache-manifest.37c38e37ac501260785153a14fbd8adb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b793aca8cb18ce4fbe2bd7c70e24629",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d64f28f439532a9885da",
+    "url": "/static/css/2.c81ee60c.chunk.css"
+  },
+  {
+    "revision": "b68fe3636e223db3498a",
+    "url": "/static/css/main.6cf7fc51.chunk.css"
+  },
+  {
+    "revision": "d64f28f439532a9885da",
+    "url": "/static/js/2.597fda88.chunk.js"
+  },
+  {
+    "revision": "b68fe3636e223db3498a",
+    "url": "/static/js/main.c19b719f.chunk.js"
+  },
+  {
+    "revision": "c20bda0da91af47bb8f6",
+    "url": "/static/js/runtime-main.8fc0d6a4.js"
+  },
+  {
+    "revision": "6dd346f00ae97ad5ad3d64b73ebf2e53",
+    "url": "/static/media/404.6dd346f0.png"
+  },
+  {
+    "revision": "ed80d514acac55079615f19ce04c0c08",
+    "url": "/static/media/bg.ed80d514.png"
+  }
+]);
