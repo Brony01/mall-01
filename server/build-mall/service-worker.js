36a06787a02b74728cb8516518ad11f5d48fddc5
Zengquan Liang <21301042@bjtu.edu.cn>
2024-06-04 01:37:08 +0800

    修复了一些bug

--- server/build-mall/service-worker.js
+++ server/build-mall/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./login/precache-manifest.2f73dc28e6e477e6d05088c4c5e54950.js"
+  "/precache-manifest.cdfdfdd945e49dcb3ff2acfe25f03664.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./login/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
