b6220f28c679d3f6100ae76d5e9a2c825c49801e
Zengquan Liang <21301042@bjtu.edu.cn>
2024-06-04 22:25:53 +0800

    修复了一些bug

---
+++ server/build-mall/precache-manifest.cdfdfdd945e49dcb3ff2acfe25f03664.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05cec557c3986a386d5d7ae24d6d25da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fa4fd99c8fb24edc432",
+    "url": "/static/css/2.9cf01dcc.chunk.css"
+  },
+  {
+    "revision": "256f3a9ec4b5f91327a3",
+    "url": "/static/css/main.00065917.chunk.css"
+  },
+  {
+    "revision": "0fa4fd99c8fb24edc432",
+    "url": "/static/js/2.fa46de04.chunk.js"
+  },
+  {
+    "revision": "256f3a9ec4b5f91327a3",
+    "url": "/static/js/main.8f5504f8.chunk.js"
+  },
+  {
+    "revision": "c20bda0da91af47bb8f6",
+    "url": "/static/js/runtime-main.8fc0d6a4.js"
+  },
+  {
+    "revision": "6dd346f00ae97ad5ad3d64b73ebf2e53",
+    "url": "/static/media/404.6dd346f0.png"
+  },
+  {
+    "revision": "ed80d514acac55079615f19ce04c0c08",
+    "url": "/static/media/bg.ed80d514.png"
+  }
+]);
